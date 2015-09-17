f6728154788a0813978c7425537f8871dc91a304
Miracle Max <miraclemax@nonexistant.com>
2015-09-17 00:40:01 +0000

    Fleshed out encoding specs, started on implementation

--- src/tx_encoder.js
+++ src/tx_encoder.js
@@ -1,6 +1,11 @@
+var util = require('util')
+var bitcore = require('bitcore')
 var network = require('./network')
 
-var PREFIX = 'CNTRPRTY'
+var Address = bitcore.Address
+var PublicKey = bitcore.PublicKey
+
+var DEFAULT_PREFIX = 'CNTRPRTY'
 
 var P2PKH = 'OP_DUP OP_HASH160 %s OP_EQUALVERIFY OP_CHECKSIG'
 var MULTISIG = '1 %s %s OP_CHECKMULTISIG'
@@ -13,18 +18,150 @@ var BYTES_IN_MULTISIG = (33 * 2) - 1 - 2 - 2
 var BYTES_IN_PUBKEYHASH = 20 - 1
 var BYTES_IN_OPRETURN = 40
 
+util.inherits(TxEncoderError, Error)
+
+function TxEncoderError (message) {
+  this.name = this.constructor.name
+  this.message = 'Transaction encoder error: ' + message
+  Error.captureStackTrace(this, this.constructor)
+}
+
+function BadEncodingError () {
+  TxEncoderError.call(this, 'invalid parameters')
+}
+
 function TxEncoder (key, data, options) {
   this.key = key
   this.data = data
 
   options = options || {}
+  this.prefix = options.prefix || DEFAULT_PREFIX
   this.network = options.network || network.main
+
+  this.encryptKey = key
+  this.sourceDate = data
+
+  if (options.senderPubkey) {
+    this.senderPubkey = options.senderPubkey
+    this.senderAddr = Address.fromPublicKey(new PublicKey(this.senderPubkey))
+  } else if (options.senderAddr) {
+    this.senderAddr = options.senderAddr
+  } else {
+    // Missing Sender Address:
+    throw new BadEncodingError()
+  }
+
+  if (options.receiverAddr) {
+    this.receiverAddr = options.receiverAddr
+  }
+}
+
+/* Take a too short data pubkey and make it look like a real pubkey.
+ * Take an obfuscated chunk of data that is two bytes too short to be a pubkey and
+ * add a sign byte to its beginning and a nonce byte to its end. Choose these
+ * bytes so that the resulting sequence of bytes is a fully valid pubkey (i.e. on
+ * the ECDSA curve). Find the correct bytes by guessing randomly until the check
+ * passes. (In parsing, these two bytes are ignored.)
+ *
+ * NOTE: This function is named "make_fully_valid" in the official code. */
+TxEncoder.prototype.dataToPubkey = function (bytes) {
+  /*
+  raise InvalidPubkey unless bytes.length == 31
+
+  random_bytes = Digest::SHA256.digest bytes
+
+  # Deterministically generated, for unit tests.
+  sign = (random_bytes[0].ord & 1) + 2
+  nonce = initial_nonce = random_bytes[1].ord
+
+  begin
+    nonce += 1
+    next if nonce == initial_nonce
+
+    ret = (sign.chr + bytes + (nonce % 256).chr).unpack('H*').first
+
+  # Note that 256 is the exclusive limit:
+  end until Bitcoin.valid_pubkey? ret
+
+  # I don't actually think this is ever possible. Note that we return 66 bytes
+  # as this is string of hex, and not the bytes themselves:
+  raise InvalidPubkeyGenerated unless ret.length == 66
+
+  ret
+  */
+}
+
+/* This is a little helper method that lets us split our binary data into 
+ * chunks for further processing */
+TxEncoder.prototype.collectChunks = function (data,chunk_length, block) {
+  /*
+  (source_data.length.to_f / chunk_length).ceil.times.collect{|i| 
+    block.call source_data.slice(i*chunk_length, chunk_length) }
+  */
+}
+
+TxEncoder.prototype.p2pkhWrap = function (operation) {
+  /*
+  [ (receiver_addr) ? P2PKH % Bitcoin.hash160_from_address(receiver_addr) : nil, 
+    operation,
+    P2PKH % Bitcoin.hash160_from_address(sender_addr) ].flatten.compact
+  */
+}
+
+TxEncoder.prototype.encrypt = function (chunk) {
+  /*
+  RC4.new(encrypt_key).encrypt chunk
+  */
 }
 
 TxEncoder.prototype.toOpMultisig = function () {
   return null
+
+  /*
+  raise MissingPubkey unless sender_pubkey
+
+  data_length = BYTES_IN_MULTISIG-prefix.length
+  p2pkh_wrap collect_chunks(source_data,data_length){|chunk| 
+    padding = 0.chr * (data_length-chunk.length)
+
+    data = encrypt [(chunk.length+prefix.length).chr,prefix, chunk, padding].join
+
+    data_keys = [(0...31), (31...62)].collect{|r| data_to_pubkey data[r] }
+
+    MULTISIG % [(data_keys + [sender_pubkey]).join(' '), 3]
+  }
+  */
+}
+
+TxEncoder.prototype.toOpReturn = function () {
+  return null
+  /*
+  # I'm fairly certain that using more than one OP_RETURN per transaction is
+  # unstandard behavior right now
+  raise DataTooLarge if (source_data.length + prefix.length) > BYTES_IN_OPRETURN
+
+  data = encrypt [prefix,source_data].join
+
+  p2pkh_wrap( OPRETURN % data.unpack('H*').first )
+  */
+}
+
+TxEncoder.prototype.toOpCheckSig = function () {
+  return null
+  /*
+  p2pkh_wrap collect_chunks(source_data, BYTES_IN_PUBKEYHASH-prefix.length){ |chunk|
+    data_length = prefix.length + chunk.length
+
+    padding = 0.chr * (BYTES_IN_PUBKEYHASH - data_length)
+
+    enc_chunk = encrypt [(data_length).chr, prefix, chunk, padding].join
+
+    P2PKH % enc_chunk.unpack('H*').first
+  }
+  */
 }
 
 module.exports = {
-  TxEncoder: TxEncoder
+  TxEncoder: TxEncoder,
+  BadEncodingError: BadEncodingError
 }
--- test/tx_encoder.js
+++ test/tx_encoder.js
@@ -17,7 +17,72 @@ describe('TxEncode', function () {
       { receiverAddr: "12pv1K6LTLPFYXcCwsaU7VWYRSX7BuiF28",
         senderPubkey: '02f4aef682535628a7e0492b2b5db1aa312348c3095e0258e26b275b25b10290e6' } )
 
-    assert.deepEqual(encoder.toOpMultisig, 
+    assert.deepEqual(encoder.toOpMultisig(), 
       fixtures.asm.getSampleCounterpartyTransactionProtocol2)
+  })
+
+  it('Tokenly\'s getSampleCounterpartyTransactionProtocol3()', function () {
+    var encoder = new TxEncoder(new Buffer([ 231, 249, 49, 157, 232, 86, 97, 
+      195, 49, 48, 232, 11, 125, 241, 103, 51, 22, 72, 146, 238, 178, 225, 251,
+       4, 78, 90, 197, 222, 122, 50, 105, 183 ]),
+      new Buffer(fixtures.decoded.getSampleCounterpartyTransactionProtocol3),
+      { receiverAddr: "1FEbYaghvr7V53B9csjQTefUtBBQTaDFvN",
+        senderPubkey: '0257b0d96d1fe64fbb95b2087e68592ee016c50f102d8dcf776ed166473f27c690' } )
+
+    assert.deepEqual(encoder.toOpMultisig(), 
+      fixtures.asm.getSampleCounterpartyTransactionProtocol3)
+  })
+
+  it('Tokenly\'s getSampleCounterpartyTransactionProtocol4()', function () {
+    var encoder = new TxEncoder(new Buffer([ 166, 237, 184, 97, 113, 252, 119, 
+      54, 56, 159, 32, 188, 99, 84, 34, 85, 219, 191, 162, 220, 209, 115, 252, 
+      7, 148, 20, 173, 171, 127, 84, 128, 113]),
+      new Buffer(fixtures.decoded.getSampleCounterpartyTransactionProtocol4),
+      { receiverAddr: "1Q7VHJDEzVj7YZBVseQWgYvVj3DWDCLwDE",
+        senderAddr: '1MFHQCPGtcSfNPXAS6NryWja3TbUN9239Y' } )
+
+    assert.deepEqual(encoder.toOpReturn(), 
+      fixtures.asm.getSampleCounterpartyTransactionProtocol4)
+  })
+
+  it('Tokenly\'s getSampleCounterpartyTransactionProtocol5()', function () {
+    var encoder = new TxEncoder(new Buffer([ 207, 143, 162, 156, 246, 57, 56, 
+      167, 135, 231, 50, 200, 80, 113, 105, 86, 104, 185, 224, 11, 254, 223,
+      248, 7, 223, 189, 200, 90, 106, 214, 166, 150 ]),
+      new Buffer(fixtures.decoded.getSampleCounterpartyTransactionProtocol5),
+      { receiverAddr: "1KUsjZKrkd7LYRV7pbnNJtofsq1HAiz6MF",
+        senderAddr: '12iVwKP7jCPnuYy7jbAbyXnZ3FxvgLwvGK' } )
+
+    assert.deepEqual(encoder.toOpReturn(), 
+      fixtures.asm.getSampleCounterpartyTransactionProtocol5)
+  })
+
+  it('encodes a four output pubkeyhash', function () {
+    /* This was from Txid:
+       76133a842ced8d76047e070924bca66652b19581803079f200d35fd824499940 */
+
+    var encoder = new TxEncoder(new Buffer([ 100, 44, 103, 30, 153, 243, 181, 
+      174, 84, 85, 10, 251, 21, 250, 195, 217, 143, 87, 80, 107, 170, 107, 190,
+      149, 250, 21, 183, 240, 124, 102, 78, 27 ]),
+      new Buffer(fixtures.decoded.pubkeyHash),
+      { receiverAddr: "1BdHqBSfUqv77XtBSeofH6XwHHczZxKRUF",
+        senderAddr: '1Ko36AjTKYh6EzToLU737Bs2pxCsGReApK' } )
+
+    assert.deepEqual(encoder.toOpCheckSig(), 
+      fixtures.asm.pubkeyHash)
+  })
+
+  it('Encodes the gigantic multsig satoshi broadcast', function () {
+    /* This was from Txid:
+       14200afba2c8f91664afc37143763e5987a20647db3443c999137cc41b4db6e4 */
+
+    var encoder = new TxEncoder(new Buffer([ 19, 162, 25, 76, 71, 71, 199, 103, 
+      133, 167, 78, 254, 81, 184, 120, 121, 71, 22, 69, 221, 193, 38, 141, 233, 
+      202, 166, 37, 243, 169, 163, 5, 69 ]),
+      new Buffer(fixtures.decoded.motherOfMultisig),
+      { senderPubkey: '02a51147c9e3a554ed35e20cc5ca0fef20e47ae976cfe06a594e135e416bb05e32' } )
+
+    assert.deepEqual(encoder.toOpMultisig(), 
+      fixtures.asm.motherOfMultisig)
   })
 })
