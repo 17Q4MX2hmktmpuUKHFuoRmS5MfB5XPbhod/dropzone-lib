d0cc53b12cac1db578ec7e14cbd5d32e62727bc4
Aynik <aynik26@gmail.com>
2015-10-12 03:42:58 +0200

    Added multiplatform patching via jsdiff

--- scripts/postinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-patch -s -p1 -d node_modules/bitcore < patches/bitcore.patch
-patch -s -p1 -d node_modules/bitcore-p2p < patches/bitcore-p2p.patch
-
-patch -s node_modules/commander/index.js < patches/commander.patch
-patch -s node_modules/bitcore-p2p/node_modules/buffers/index.js < patches/buffers.patch
-patch -s node_modules/throbber/index.js < patches/throbber.patch
-patch -s node_modules/migrate-orm2/index.js < patches/migrate-orm2.patch
-patch -s node_modules/orm/lib/Utilities.js < patches/orm-utilities.patch
