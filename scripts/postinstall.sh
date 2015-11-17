47267f0a1cdf695536cee5d72209130a27831cde
aynik <aynik26@gmail.com>
2015-11-18 00:37:58 +0100

    Replaced bash function call for inline find script, fixes #11

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
-
-apply_patch () {
-  PATCH="$*"
-  TARGET="${PATCH%/*}"
-  RELTARGET=node_modules/"${TARGET#patches/}"
-  patch -l "$RELTARGET" "$PATCH"
-}
-
-export -f apply_patch
-
 find patches -iname 'patch' \
-  -exec bash -c 'apply_patch "{}"' \;
-
-echo
+  -exec sh -c '
+    PATCH="{}"
+    TARGET="${PATCH%/*}"
+    RELTARGET=node_modules/"${TARGET#patches/}"
+    patch -l "$RELTARGET" "$PATCH"
+  ' \;
