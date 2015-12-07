987aac303e96525ff19554292d25c509f427e1b6
Chris DeRose <chris@chrisderose.com>
2015-12-06 19:45:10 -0500

    Major refactoring of chat sessions. Implemented working chat/session test

--- test/session.js
+++ test/session.js
@@ -1,22 +1,20 @@
 /* global describe it before after */
 /* eslint no-new: 0 */
 
-var assert = require('assert')
+var chai = require('chai')
 var crypto = require('crypto')
 var async = require('async')
 var bitcore = require('bitcore-lib')
 
+var expect = chai.expect
+
 var fakeConnection = require('../lib/drivers/fake')
 var session = require('../lib/session')
-var messages = require('../lib/messages')
 
 var globals = require('./fixtures/globals')
 
 var PrivateKey = bitcore.PrivateKey
-var testnet = bitcore.Networks.testnet // TODO: nix?
-
 var Session = session.Session
-var Chat = messages.Chat
 
 describe('Session', function () {
   var connection = null
@@ -37,15 +35,15 @@ describe('Session', function () {
 
   it('performs a simple non-deterministic chat', function (nextSpec) {
     // The entropy generation takes a bit:
-    this.timeout(10000)
+    this.timeout(15000)
 
     // New Buyer to Seller Connection:
     var testerPrivKey = PrivateKey.fromWIF(globals.testerPrivateKey)
     var tester2PrivKey = PrivateKey.fromWIF(globals.tester2PrivateKey)
     var tester2PubKey = globals.tester2PublicKey
 
-    var buyerSecret = crypto.randomBytes(128).toString('hex')
-    var sellerSecret = crypto.randomBytes(128).toString('hex')
+    var buyerSecret = crypto.randomBytes(128).toString('hex') // TODO: These shouldn't be hex
+    var sellerSecret = crypto.randomBytes(128).toString('hex') // TODO: These shouldn't be hex
 
     async.series([
       function (next) {
@@ -53,42 +51,49 @@ describe('Session', function () {
         var buyerToSeller = new Session(connection, testerPrivKey, buyerSecret,
           {receiverAddr: tester2PubKey})
 
-        buyerToSeller.authenticate(function(err, chatInit) { 
-          next(null, buyerToSeller) // TODO: can we pass this instead? 
+        buyerToSeller.authenticate(function (err, chatInit) {
+          if (err) throw err
+          next(null, buyerToSeller) // TODO: can we pass this instead?
         })
       }, function (next) {
         // Seller initiates Authentication To Buyer
-        var sellerToBuyer
-
         Session.all(connection, tester2PubKey, function (err, sessions) {
           if (err) return next(err)
-          if (sessions.length == 0) return next("Couldn't retrieve session")
+          if (sessions.length === 0) return next('Couldn\'t retrieve session')
 
           var sellerToBuyer = new Session(connection, tester2PrivKey, sellerSecret,
             {withChat: sessions[0]})
 
-          sellerToBuyer.authenticate(function(err, chatAuth) {
+          sellerToBuyer.authenticate(function (err, chatAuth) {
+            if (err) return next(err)
             next(null, sellerToBuyer) // TODO: Can we pass this instead?
           })
         })
-      }], function(err, sessions){
-        buyerToSeller = sessions[0]
-        sellerToBuyer = sessions[1]
+      }],
+      function (err, sessions) {
+        if (err) throw err
+        var buyerToSeller = sessions[0]
+        var sellerToBuyer = sessions[1]
 
         async.series([
-          function (next) { sellerToBuyer.send("Hello Buyer", next) },
-          function (next) { buyerToSeller.send("Hello Seller", next) },
+          function (next) { sellerToBuyer.send('Hello Buyer', next) },
+          function (next) { buyerToSeller.send('Hello Seller', next) },
           function (next) { sellerToBuyer.communications(next) },
           function (next) { buyerToSeller.communications(next) }
-        ],function(err, communications) {
-          sellerToBuyerContents = communications[2].map(function (comm) { 
-            return comm.contentsPlain()})
-          buyerToSellerContents = communications[3].map(function (comm) { 
-            return comm.contentsPlain()})
+        ],
+        function (err, communications) {
+          if (err) throw err
 
-          expect(sellerToBuyerContents).to.deep.equal(["Hello Seller", "Hello Buyer"])
-          expect(buyerToSellerContents).to.deep.equal(["Hello Seller", "Hello Buyer"])
+          var sellerToBuyerContents = communications[2].map(
+            function (comm) { return comm.contentsPlain() })
+          var buyerToSellerContents = communications[3].map(
+            function (comm) { return comm.contentsPlain() })
+
+          expect(sellerToBuyerContents).to.deep.equal(['Hello Seller', 'Hello Buyer'])
+          expect(buyerToSellerContents).to.deep.equal(['Hello Seller', 'Hello Buyer'])
+
+          nextSpec()
         })
-    })
+      })
   })
 })
