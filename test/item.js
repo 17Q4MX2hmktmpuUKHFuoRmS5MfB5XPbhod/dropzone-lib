1857204cb6dead0b29f22969c60b310f514aaaf9
Scrooge McDuck, But With Bitcoin <scrooge@duckbankbutwithbitcoin.com>
2015-12-17 00:17:27 -0500

    Added additional tests

--- test/item.js
+++ test/item.js
@@ -41,4 +41,27 @@ describe('Item', function () {
     expect(item.receiverAddr).to.equal('mfZ1415XX782179875331XX1XXXXXgtzWu')
     expect(item.senderAddr).to.be.undefined
   })
+
+  describe("burn addresses", function () {
+    it ("supports 6 digit distances", function () {
+      [90, 0, -90, 51.500782,-51.500782].forEach( function (lat) {
+        [90, 0, -90, 51.500782,-51.500782].forEach( function (lon) {
+          [9,8,5,2,0,101,11010,999999,100000].forEach( function (radius) {
+            var addr = chai.factory.create('item', connection, {radius: radius, 
+               latitude: lat, longitude: lon}).receiverAddr
+
+            expect(addr.length).to.equal(34)
+
+            var parts = /^mfZ([0-9X]{9})([0-9X]{9})([0-9X]{6})/.exec(addr)
+            parts = parts.map(function (p) { 
+              return parseInt(p.replace(/X/g, 0))})
+
+            expect(parts[1]).to.equal(Math.floor((lat+90) * 1000000))
+            expect(parts[2]).to.equal(Math.floor((lon+180) * 1000000))
+            expect(parts[3]).to.equal(radius)
+          })
+        })
+      })
+    })
+  })
 })
