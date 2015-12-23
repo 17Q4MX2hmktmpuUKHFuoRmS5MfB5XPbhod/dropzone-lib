1b870cd9ddbacfd00f49d41370b34567f06d6645
Chris DeRose <chris@chrisderose.com>
2015-12-23 16:34:27 -0500

    Switched to gulp, added packaging options, karma testing

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var bitcore = require('bitcore-lib')
-var cache = require('./lib/cache')
-var actions = require('./lib/actions')
 var drivers = require('./lib/drivers')
-var storage = require('./lib/storage')
 var messages = require('./lib/messages')
 var session = require('./lib/session')
-var tx_decoder = require('./lib/tx_decoder')
+var txDecoder = require('./lib/tx_decoder')
+var txEncoder = require('./lib/tx_decoder')
 var blockchain = require('./lib/blockchain')
+var listing = require('./lib/listing')
+var profile = require('./lib/profile')
+var session = require('./lib/session')
 
 module.exports = {
   bitcore: bitcore,
   actions: actions,
   drivers: drivers,
   blockchain: blockchain,
-  cache: cache,
-  storage: storage,
   messages: messages,
   session: session,
-  tx_decoder: tx_decoder
+  txDecoder: txDecoder,
+  txEncoder: txEncoder
 }
