1356c6d930d4061fdc58d1f27026bb91961aa246
Chris DeRose <chris@chrisderose.com>
2015-12-22 19:10:44 -0500

    Started removing cli code and patches

--- migrations/cache/001-add-transaction-blockid.js
+++
@@ -1,9 +0,0 @@
-exports.up = function (next) {
-  this.addColumn('transactions', {
-    blockid: { type: 'text' }
-  }, next)
-}
-
-exports.down = function (next) {
-  this.dropColumn('transactions', 'blockid', next)
-}
