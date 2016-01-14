8cb740de0666e15fd8ff71586b55bc2e084b53ba
Chris DeRose <chris@chrisderose.com>
2016-01-13 20:25:24 -0500

    Update 2016-01-14T01:25:20.604Z

--- scripts/postinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-find patches -iname 'patch' \
-  -exec sh -c '
-    PATCH="{}"
-    TARGET="${PATCH%/*}"
-    RELTARGET=node_modules/"${TARGET#patches/}"
-    patch -l "$RELTARGET" "$PATCH"
-  ' \;
-echo
